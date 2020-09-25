46aad5d0904c64190cc286eb904f0f41f4411324
AJ Leichner <leichneraj@s.dcsdk12.org>
2020-09-25 11:42:08 -0600

    Finish code for Padjen University acceptence program

--- Admissions.java
+++ Admissions.java
@@ -5,21 +5,30 @@ public class Admissions {
     public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int Elite;
-        int solid;
-        int acceptable;
-        int nope;
+        boolean accepted;
 
+        int elite = 0;
+        int solid = 0;
+        int acceptable = 0;
+        int nope = 0;
+
+        System.out.println("What is your first name?");
+        String firstName = scanner.next();
+        System.out.println("What is your last name?");
+        String lastName = scanner.next();
         System.out.println("What is your SAT score?");
         int satScore = scanner.nextInt();
-        System.out.println("What is your class rank?");
+        System.out.println("What is your class rank? The higher the number the better.");
         int classRank = scanner.nextInt();
         System.out.println("How many awards do you have?");
         int awardsAmount = scanner.nextInt();
-        System.out.println("What state do you reside in?");
+        System.out.println("What state do you reside in? Use capital letters as needed.");
+        String state = scanner.next();
+
+        scanner.close();
 
         if(satScore >= 1500) {
-            Elite++;
+            elite++;
         } else if(satScore >= 1400) {
             solid++;
         } else if(satScore >= 1200) {
@@ -29,10 +38,48 @@ public class Admissions {
         }
 
         if(classRank >= 95) {
-            Elite++;
+            elite++;
         } if(classRank >= 90) {
             solid++;
-        } // not done yet, was doing college board sign up.
+        } else if(classRank >= 85) {
+            acceptable++;
+        } else {
+            nope++;
+        }
+
+        if (awardsAmount >= 10) {
+            elite++;
+        } else if(awardsAmount >= 8) {
+            solid++;
+        } else if(awardsAmount >= 3) {
+            acceptable++;
+        } else {
+            nope++;
+        }
+
+        if(state.contains("Colorado")) {
+            elite++;
+        } else if(state.contains("Florida")) {
+            nope++;
+        } else {
+            acceptable++;
+        }
+
+
+        if(nope == 0) {
+            if(lastName.contains("Padjen")) {
+                System.out.println("\nYou have been accepted to Padjen University! Since your last name is Padjen you have instantly been accepted!");
+            } else if(elite >= 1) {
+                System.out.println("\nYou have been accepted to Padjen University! You have been found to contain " + elite + " of the elite level values. You needed only one or more and you had: " + elite);
+            } else if(solid >=2) {
+                System.out.println("\nYou have been accepted to Padjen University! You have been found to contain " + solid + " of the solid level values. You needed only two or more and you had: " + solid);
+            } else {
+                System.out.println("\nWe are all very sorry but you have not been accepted to Padjen University. This is due to the fact that you had only acceptable level skills and " + solid + " level skills.");
+            }
+
+        } else {
+            System.out.println("\nI'm sorry but you have not been accepted to Padjen University. This is due to the fact that you had " + nope + " characteristic(s) that did not meet our standards.");
+        }
 
     }
 
